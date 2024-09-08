1f39d02b8fa6deba97873c5f017543064f96cabb
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2024-09-08 12:57:31 +0530

    :zap: feeds addition

---
+++ internal/auth/auth.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+
+
+func GetAPIKey(headers http.Header) (string,error) {
+  
+  val:= headers.Get("Authorization")
+
+  if val == "" {
+      return "",errors.New("no Authentication info found")
+  }
+
+
+  vals := strings.Split(val," ")
+
+  if len(vals) !=2 {
+    return "",errors.New("incorrect Authentication Header")
+  }
+
+  if vals[0] != "ApiKey"{
+    return "",errors.New("Incorrect First Part of request")
+  }
+  
+
+  return vals[1],nil
+}
