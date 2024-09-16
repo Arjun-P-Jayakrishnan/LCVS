44f195bd9cf2a6318594b150e191a0e155a0b145
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2024-09-16 08:54:59 +0530

    :recycle: working on structure

---
+++ handler_feed_follows.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Arjun-P-Jayakrishnan/LCVS/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+
+	type parameters struct {
+		FeedId uuid.UUID `json:"feed_idstring"`	
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	feed, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+    FeedId:    params.FeedId,
+	})
+
+	if err != nil{
+		respondWithError(w, 400, fmt.Sprintf("Couldn't Create feed %v", err))
+	}
+
+  respondWithJSON(w,200,databaseFeedToFeed(feed))
+}
+
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter,r *http.Request){
+
+  feed,err := apiCfg.DB.GetFeeds(r.Context())
+  
+  if err!=nil{
+    respondWithError(w,400,fmt.Sprintf("Couldn't get feeds %v",err))
+    return
+  }
+
+  respondWithJSON(w,200,databaseFeedsToFeeds(feed))
+
+ }
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-  "database/sql"
 
 	"github.com/Arjun-P-Jayakrishnan/LCVS/internal/database"
 	"github.com/go-chi/chi"
@@ -15,16 +15,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
-
-
-
-type apiConfig struct{
-    DB *database.Queries
+type apiConfig struct {
+	DB *database.Queries
 }
 
 func main() {
 
-	godotenv.Load(".env")
+	errEnv := godotenv.Load(".env")
+
+	if errEnv != nil {
+      log.Fatal("Can't access system files")
+	}
 
 	portString := os.Getenv("PORT")
 
@@ -32,55 +33,53 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-  dbUrl :=os.Getenv("DB_URL")
-  if dbUrl == "" {
-      log.Fatal("PORT is not found in the environment")
-  }
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
 
-  conn,err:=sql.Open("postgres",dbUrl)
+	conn, err := sql.Open("postgres", dbUrl)
 
-  if err !=nil {
-      log.Fatal("Can't connect to database:")
-  }
+	if err != nil {
+		log.Fatal("Can't connect to database:")
+	}
 
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
 
-  apiCfg :=apiConfig{
-    DB:database.New(conn), 
-  }
+	router := chi.NewRouter()
 
-  router:=chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "htttp://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
-  router.Use(cors.Handler(cors.Options{
-      AllowedOrigins: []string{"https://*","htttp://*"},
-      AllowedMethods: []string{"GET","POST","PUT","DELETE","OPTIONS"},
-      AllowedHeaders: []string{"*"},
-      ExposedHeaders: []string{"Link"},
-      AllowCredentials: false,
-      MaxAge:          300,
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
-  }))
+	router.Mount("/v1", v1Router)
 
-  v1Router := chi.NewRouter()
-  v1Router.Get("/healthz",handlerReadiness)
-  v1Router.Get("/err",handlerErr)
-  v1Router.Post("/users",apiCfg.handlerCreateUser)
-  v1Router.Get("/users",apiCfg.middlewareAuth(apiCfg.handlerGetUser))
-  v1Router.Post("/feeds",apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
-  v1Router.Get("/feeds",apiCfg.handlerGetFeeds)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
 
-  router.Mount("/v1",v1Router)
+	log.Printf("Server starting at Port %v", portString)
+	errServer := srv.ListenAndServe()
 
-  srv := &http.Server{
-      Handler:router,
-      Addr:":"+portString,
-  }
-  
-  log.Printf("Server starting at Port %v",portString)
-  errServer := srv.ListenAndServe()
-
-  if errServer!=nil {
-      log.Fatal(errServer)
-  }
+	if errServer != nil {
+		log.Fatal(errServer)
+	}
 
 	fmt.Println("Port:", portString)
 }
