e0f4a681ff0edc7afd4a90a0a2cc1b084e4ccb14
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2024-10-20 14:07:52 +0530

    fix: chnages to ui

---
+++ ui/image.go
@@ -0,0 +1,21 @@
+
+
+
+//var imageData []byte
+
+var imageOp = func() paint.ImageOP{
+    
+  m,err := png.Decode(bytes.NewReader(imageData))
+  if err!=nil{
+    panic(err)
+  }
+
+  return paint.NewImage(m)
+
+}()
+
+
+func main() {
+  
+
+}
